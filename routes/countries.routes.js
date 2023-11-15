14fae9d0ce9b304f5347712090428ba12b5891f4
Valentina <invalofu.15@gmail.com>
2023-11-15 11:56:24 +0100

    update countries

--- routes/countries.routes.js
+++ routes/countries.routes.js
@@ -5,30 +5,41 @@ const countriesService = require('../services/countries.services')
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-    // res.render("countries/list.hbs")
+
+    // countriesService
+    //     .getAllCountries()
+    //     .then(({ data }) => {
+
+    //         // console.log("ESTOS SON TODOS LOS PAISES----------------------------", data[0])
+    //         data.map(elm => {
+    //             countriesService
+    //                 .getCountryDetails(elm.iso2)
+    //                 .then(({ data }) => {
+
+    //                     console.log("---------------------------", data)
+
+    //                 })
+    //                 .catch(err => next(err))
+    //         })
+    //         // console.log("------------------------------------------------", data.forEach(elm => elm.iso2))
+
+    //         // res.render("countries/list", { allCountries: data })
+    //     })
+    //     .catch(err => next(err))
+
+
     countriesService
         .getAllCountries()
-        .then(({ data }) => {
-
-            // console.log("ESTOS SON TODOS LOS PAISES----------------------------", data[0])
-            data.map(elm => {
-                countriesService
-                    .getCountryDetails(elm.iso2)
-                    .then(({ data }) => {
-
-                        console.log("---------------------------", data)
-
-                    })
-                    .catch(err => next(err))
-            })
-            // console.log("------------------------------------------------", data.forEach(elm => elm.iso2))
-
-            // res.render("countries/list", { allCountries: data })
-        })
+        .then(({ data }) => res.render("countries/list.hbs", { allCountries: data }))
         .catch(err => next(err))
+
+
 
 });
 
+router.get('/cuidades', (req, res, next) => {
+    res.render('countries/cities.hbs')
+})
 
 
 
